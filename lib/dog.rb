c19b8fc91500aad1991e2f89e6514aae273e8669
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-26 14:56:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,8 +63,8 @@ class Dog
     DB[:conn].execute(sql, id).map{|row| self.new_from_db(row)}.first
   end
 
-  def self.find_or_create_by
-
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[conn:].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
   end
 
 end
