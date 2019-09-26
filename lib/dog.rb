c6bd750e91166ba7d73e3e4b6c209575b8eb420a
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-26 15:03:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -81,4 +81,8 @@ class Dog
     DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}.first
   end
 
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
