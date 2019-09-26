e24e4cb758fd3eeb00c83b98c714bea84b5b7dcd
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-26 14:48:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,6 +37,7 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
+    self
   end
 
   def self.create(id:, name:, breed:)
